306fbb7e4ca3ad0edf9d2ae75f6fddad59fd8c5a
unknown <dev@NSN1191261221.edu119.local>
2026-02-27 09:19:32 -0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <main>
+      <h1>Seja bem-vindo João</h1>
+
+      <input type="email" placeholder="Example@gmail.com" required />
+      <input type="password" placeholder="*********" required />
+
+      <button type="submit">confirmar</button>
+    </main>
+  )
+}
+
+export default App;
